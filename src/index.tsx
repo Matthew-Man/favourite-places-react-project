6e70b0c7bf4ce27c6737bc2b120c070547c61c77
Matthew Man <matthew08m@gmail.com>
2021-02-09 15:22:11 +0000

    added Footer React funtion

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 function Header() {
   return (
-    <div style={{backgroundColor: "#3f4452", padding: "10px"}}>
+    <header style={{backgroundColor: "#3f4452", padding: "10px"}}>
       <h1 style ={{color:"white", textAlign: "center"}}>My Favourite Places</h1>
-    </div>
+    </header>
+  )
+};
+
+
+function Footer() {
+  return (
+    <footer style={{backgroundColor: "#3f4452", padding: "10px"}}>
+      <h1 style ={{color:"white", textAlign: "center"}}>Footer Stuff Here</h1>
+    </footer>
   )
 }
 
-ReactDOM.render(Header(), document.getElementById('header'));
+
+ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
