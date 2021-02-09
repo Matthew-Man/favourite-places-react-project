c09f7a18ce70e471c59e233ee8d7a1623c19d8cb
Matthew Man <matthew08m@gmail.com>
2021-02-09 14:39:08 +0000

    React header function created and test render successful

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Header() {
+  return (
+    <h1>Test this works</h1>
+  )
+}
 
-
+ReactDOM.render(Header(), document.getElementById('header'));
